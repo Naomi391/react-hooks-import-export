622fc670369fc8a3d43f55367e4711ecde58f285
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-01-30 02:34:31 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+// import MesaVerde from "./parks/MesaVerde";
+// import { trees, wildlife } from ".parks/elevation";
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
